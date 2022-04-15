09985c68e514118a8c0c1c8ed9f32329c1a91426
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2022-04-15 17:10:03 +0200

    Some changes

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Iperf3 service with nodes callable by http API
 
 See docker-compose.yml
 
-For the moment, each node is a server and client.
+## Example of use
 
 - Run this app on node1
 - Run this app on node2
 - Call http://node1/node2 to get the result
 - or call http://node2/node1 !
 
-If needed, we can add feature flags to run only server or client part (for example is you have masterNode and slaveNode1, slaveNode2, you maybe want to have servers on slaves and client on master)
+## Configuration
 
-Configuration can be added to configure that, ports, logs, etc
+Please see index.js, "schema" section.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const loadConfig = require('js-libs/config').default
+const loadConfig = require('js-libs/config').default
 const runProcess = require('js-libs/process').default
 const createLogger = require('js-libs/logger').default
 const { handleExitSignals } = require('js-libs/exit-handle')
@@ -6,38 +6,92 @@ const HttpServer = require('js-libs/http-server').default
 const { once } = require('events')
 const logger = createLogger('info')
 
-const iperfServerProcess = runProcess({
-    cmd: 'iperf3',
-    args: ['-s'],
-    logger
-})
-
-const httpServer = new HttpServer({
-    logger,
-    port: 80,
-    api: {
-        routes: [{
-            method: 'get',
-            path: '/:host',
-            async handler(req, res) {
-                const requestProcess = runProcess({
-                    cmd: 'iperf3',
-                    args: ['--json', '-c', req.params.host, '-t', '2'],
-                    logger,
-                    outputType: 'json'
-                })
-
-                const [result] = await once(requestProcess, 'finish')
-
-                res.json(result)
+const config = loadConfig({
+    schema: {
+        type: 'object',
+        properties: {
+            server: {
+                type: 'object',
+                properties: {
+                    enabled: { type: 'boolean' }
+                }
+            },
+            client: {
+                type: 'object',
+                properties: {
+                    enabled: { type: 'boolean' },
+                    http: {
+                        type: 'object',
+                        properties: {
+                            port: { type: 'integer' }
+                        }
+                    }
+                }
+            },
+            iperf: {
+                type: 'object',
+                properties: {
+                    time: { type: 'integer' }
+                }
             }
-        }]
+        }
+    },
+    defaultValues: {
+        'server.enabled': true,
+        'client.enabled': true,
+        'client.http.port': 80,
+        'iperf': {}
     }
 })
 
-httpServer.start()
+logger.info('Loaded config', {config})
+
+let iperfServerProcess
+let httpServer
+
+if (config.server.enabled) {
+    logger.info('Starting server')
+    iperfServerProcess = runProcess({
+        cmd: 'iperf3',
+        args: ['-s'],
+        logger
+    })
+}
+
+if (config.client.enabled) {
+    logger.info('Starting client')
+    httpServer = new HttpServer({
+        logger,
+        port: config.client.http.port,
+        api: {
+            routes: [{
+                method: 'get',
+                path: '/:host',
+                async handler(req, res) {
+                    const requestProcess = runProcess({
+                        cmd: 'iperf3',
+                        args: [
+                            '--json',
+                            '-c', req.params.host,
+                            ...config.iperf.time ? ['-t', config.iperf.time.toString()]: []
+                        ],
+                        logger,
+                        outputType: 'json'
+                    })
+
+                    const [result] = await once(requestProcess, 'finish')
+
+                    res.json(result)
+                }
+            }]
+        }
+    })
+
+    httpServer.start()
+}
+
 
 handleExitSignals(() => {
-    iperfServerProcess.abort()
-    httpServer.stop()
+    iperfServerProcess && iperfServerProcess.abort()
+    httpServer && httpServer.stop()
 })
